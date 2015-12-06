dff716a96ca6d18f6a9e14ada1b1fab0a409b3fc
Robert Kramer <kramer102@gmail.com>
2015-12-05 23:10:45 -0800

    Just a starting point.  read_csv crashed my comp

---
+++ project_scratch.R
@@ -0,0 +1,7 @@
+setwd("/home/robert/all_Robert_asus/prog_asus/R/Project") 
+## libraries used
+library(readr)
+
+## just going to add the first bit
+first-look <- read_csv("./Data/output/Scorecard.csv")
+
