8a1f350b0182124578d334554a59353892a09136
Robert Kramer <kramer102@gmail.com>
2015-12-07 11:59:37 -0800

    added a field sat to the earnings data from the example.. use spreadsheet to see entire thing.  really helpful for understanding what to query from database

--- project_scratch.R
+++ project_scratch.R
@@ -5,6 +5,7 @@ library(RSQLite)
 library(dplyr) #takes a while to install
 library(tidyr)
 library(ggvis)
+library(ggplot2)
 
 ## just going to add the first bit
 
@@ -27,6 +28,9 @@ tables <- tables %>%
 
 print.table(tables)
 
+## gave me all the column names and types
+test <- dbGetQuery(db, 'PRAGMA table_info(Scorecard)')
+
 dbGetQuery(db, "SELECT Year, COUNT(Id) NumSchools FROM Scorecard GROUP BY Year") %>%
   ggvis(~Year, ~NumSchools) %>%
   layer_bars()
@@ -36,7 +40,7 @@ dbGetQuery(db, "SELECT Year, COUNT(Id) NumSchools FROM Scorecard GROUP BY Year")
 #                        SELECT s11.INSTNM College,
 #                               s11.CONTROL CollegeType")
 
-## example
+## modified example
 earnings <- dbGetQuery(db, "
 SELECT s11.INSTNM College,
                        s11.CONTROL CollegeType,
@@ -44,13 +48,15 @@ SELECT s11.INSTNM College,
                        s11.pct10_earn_wne_p10 e10,
                        s11.pct25_earn_wne_p10 e25,
                        s11.pct75_earn_wne_p10 e75,
-                       s11.pct90_earn_wne_p10 e90
+                       s11.pct90_earn_wne_p10 e90,
+                       s11.SAT_AVG_ALL sat
                        FROM Scorecard s11
                        -- We have to do a self-join because the CCBASIC metadata is only attached to 2013 data
                        -- And 2013 data has no 10 year out earnings data
                        INNER JOIN Scorecard s13 ON s11.UNITID=s13.UNITID
                        WHERE s11.Year=2011
                        AND s13.Year=2013
+                       AND s11.SAT_AVG_ALL IS NOT NULL
                        AND s11.pct75_earn_wne_p10 IS NOT NULL
                        AND s11.pct75_earn_wne_p10 != 'PrivacySuppressed'
                        AND s11.PREDDEG = 'Predominantly bachelor''s-degree granting'
@@ -61,6 +67,9 @@ earnings <- cbind(Rank=1:nrow(earnings), earnings)
 earnings$College <- paste(earnings$Rank, earnings$College, sep=". ")
 earnings$College <- factor(earnings$College, levels=rev(earnings$College))
 
+m1 <- lm(e90 ~ sat,data=earnings)
+summary(m1)
+qplot(sat,e90,data = earnings)
 ### End Example
 
 ### basic sqlite from r-bloggers example
